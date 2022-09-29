921db1ada7f2cac89cd0ab009d08d9c96746dd3e
JoanGuinart <guinartcasasjoan@gmail.com>
2022-09-29 09:41:17 +0200

    StyleComponentsApply(1)

--- src/components/escena/Botones.js
+++ src/components/escena/Botones.js
@@ -1,25 +1,10 @@
+import { Boton } from "./../../styled" 
+
+
 export const Botones = (props) => {
   
-  const textoBoton = props.text
-  let fraseSelecionada = 0;
-
-  const handleButton = (textoBoton) => {
-    //e.preventDefault();
-    if (textoBoton === 'Sigiente' && fraseSelecionada === 3) {
-      alert('Estas en la última')
-    }
-    if (textoBoton === 'Anterior' && fraseSelecionada === 0) {
-      alert('Estas en la primera')
-    }
-    if (textoBoton === 'Sigiente') {
-      fraseSelecionada += 1;
-    }
-    if (textoBoton === 'Anterior') {
-      fraseSelecionada -= 1;
-    }
-  }
-
+   const textoBoton = props.text
   return (
-    <button onClick={handleButton(textoBoton)}>{textoBoton}</button>
+    <Boton type="button">{textoBoton}</Boton>  
   )
 }
