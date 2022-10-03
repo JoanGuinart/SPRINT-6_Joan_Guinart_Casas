f0958785781fc6caac0f236f49130c761be5b17a
JoanGuinart <guinartcasasjoan@gmail.com>
2022-10-03 09:34:14 +0200

    Ja funciona el nivell 1 sencer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { useState } from "react";
+import { Boton } from "./styled";
 import { Escena } from "./components/escena/Escena";
-import { Botones } from "./components/escena/Botones";
 import { frases } from "./data/frases";
-/* import React, {useState, useEffect} from "react"; */
 
 
+const App = () => {
 
+  const [numFrase, setNumFrase] = useState(0)
 
-const App = () => { 
+  const handleButton = (e, textoBoton) => {
+
+    if (textoBoton === 'Siguiente' && numFrase >= 3) { 
+      return alert('Estas en la última')
+    }
+    if (textoBoton === 'Anterior' && numFrase <= 0) {
+      return alert('Estas en la primera')
+    }
+    if (textoBoton === 'Siguiente' && numFrase !== 3) {
+      console.log(numFrase + 1);
+      return setNumFrase(numFrase + 1);
+    }
+    if (textoBoton === 'Anterior' && numFrase !== 0) {
+      console.log(numFrase - 1);
+      return setNumFrase(numFrase - 1);
+    }
+  }
 
 
   return (
-    <div>   
-      <Botones text="Anterior"/>
-      <Botones text="Siguiente"/>    
-     
-      <Escena
-       f = {frases}/>
-    
-      
+    <div>
+      <Boton type="button" onClick={(e) => handleButton(e, "Anterior")}>Anterior</Boton>
+      <Boton type="button" onClick={(e) => handleButton(e, "Siguiente")}>Siguiente</Boton>
+      <Escena f={frases} num={numFrase}/>
     </div>
   );
 }
