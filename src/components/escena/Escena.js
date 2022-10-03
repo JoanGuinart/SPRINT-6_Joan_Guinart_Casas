0471c18b069bd5541ed524f6312ebb757be71b32
JoanGuinart <guinartcasasjoan@gmail.com>
2022-10-03 10:49:39 +0200

    Comentaris afegits

--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,13 +1,15 @@
 import './escena.css';
 import { BordeFrase } from '../../styled';
 export const Escena = (props) => {
+    console.log('______', props)
     
     const num = props.num
-    const frases = (props.f).map((frase, index) => 
+    const frases = (props.frases).map((frase, index) => 
         <p key={index} className={`${index === num ? "selected": ""}`}>
             {frase}
         </p>
     )
+
 
     return (
         
